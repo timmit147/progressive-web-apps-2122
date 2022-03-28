1630785a2ae269769e254df7eb8da9bacb8609f8
timmit147 <timmeeuwsen@hotmail.nl>
2022-03-28 14:52:30 +0200

    manifest werkt

--- public/js/sw.js
+++ public/js/sw.js
@@ -1,8 +1,8 @@
 self.addEventListener('install', function (event) {
     event.waitUntil(
-      caches.open('v1').then(function (cache) {
+      caches.open('v2').then(function (cache) {
         return cache.addAll([
-          '/public/',
+          '/public',
           '/css/style.css'
         ]);
       }),
